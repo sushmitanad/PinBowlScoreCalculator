f564da451c374fc4b2d965c0027fccfd26b1b1bc
Sushmita <nad.sushmita@rediffmail.com>
2018-04-13 22:37:53 +0530

    ID-1 | Sushmita | added test for score zero

---
+++ PinBowlingScoreCalculator/PinBowlingScoreCalculator.Tests/ZeroScoreTest.cs
@@ -0,0 +1,37 @@
+﻿using PinBowlingScoreCalculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PinBowlingScoreCalculator.Tests
+{
+    public class ZeroScoreTest
+    {
+        private readonly IGameScore gameScore;
+
+        public ZeroScoreTest()
+        {
+            var frames = new List<Frame>();
+            for (int i = 0; i < 10; i++)
+                frames.Add(new Frame { CurrentBowlScore = new List<string>(new[] { "-", "-" }) });
+            var gameFrame = new GameFrame
+            {
+                FrameId = Guid.NewGuid(),
+                frames = frames
+            };
+
+            gameScore = new GameScore(gameFrame);
+        }
+
+        [Fact]
+        public async void CheckIfScoreCalculatedIsZero()
+        {
+            var expectedScore = 0;
+
+            var actualScore = await gameScore.CalculateScore();
+
+            Assert.Equal(expectedScore, actualScore);
+        }
+    }
+}
